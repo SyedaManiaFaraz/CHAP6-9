5dd8a4fd18fd8d52b240ced6ea5ce6aed024db84
SyedaManiaFaraz <syedamaniafaraz@gmail.com>
2024-08-30 01:01:58 +0500

    added index.html and index.js

---
+++ index.js
@@ -0,0 +1,112 @@
+// QUESTION NO 1
+var message = "<h1>Question No 01</h1>\n<h2>Result</h2>"
+document.write(message)
+var message = "The value of a is : "
+document.write(message)
+var a = 10;
+document.write(a)
+var message = "<br>..............................................."
+document.write(message)
+
+var message = " <br><br>The value of ++a is: "
+document.write(message)
+
+var a = 10
+var message = ++a;
+document.write(message)
+
+var message = "<br> Now the value of a is: 11"
+document.write(message)
+
+
+var num1 =("<br><br> The value of a++ is: ");
+var x = 11
+document.write(num1 + " " + x)
+var message =("<br>Now the value of a is: 12");
+document.write(message)
+
+
+var num1 =("<br><br> The value of --a is: ");
+var x = 11
+document.write(num1 + " " + x)
+var message =("<br>Now the value of a is: 11");
+document.write(message)
+
+
+var num1 =("<br><br> The value of a-- is: ");
+var x = 11
+document.write(num1 + " " + x)
+var message =("<br>Now the value of a is: 10");
+document.write(message)
+
+
+//  Question No 2
+var message = " <h1><br>Question No 02</h1>"
+document.write(message)
+var a = 2;
+var b = 1;
+
+var result = --a - --b + ++b + b--;
+document.write(result)
+
+var alpha = --a;
+document.write(alpha)
+
+var alpha = --a - --b;
+document.write(alpha)
+
+var alpha = --a - --b + ++b;;
+document.write(alpha)
+
+var alpha = --a - --b + ++b + b--;
+document.write(alpha)
+
+
+// Question No 3
+var message="<h1>Question No 03</h1>"
+document.write(message)
+var name = prompt("Enter your name");
+ if (name) {
+     alert("Hello,"+" " +" ! Nice to meet you. ");
+  } else {
+     alert ("You didn't enter a name");
+}
+
+//Question No 5
+var message = "<h1>Question No 05</h1>"
+document.write(message)
+
+var name = prompt("Enter a number");
+ if (name) {
+     alert(" WELCOME ");
+ }
+var message = "5*1=5<br>5*2=10<br>5*3=15<br>5*4=20<br>5*5=25<br>5*6=30<br>5*7=35<br>5*8=40<br>5*9=45<br>5*10=50";
+document.write(message)
+
+
+
+// Question No 6
+var message = "<h1>Question No 06</h1>"
+document.write(message)
+
+var name1 = "ENGLISH"
+var num1 = 100;
+var num2 = 52;
+var num3 = "54%";
+document.write(name1 + " " + num1 + " " + num2 + " " + num3)
+
+var name1 = "<br> MATH"
+var num1 = 100;
+var num2 = 52;
+var num3 = "54%";
+document.write(name1 + " " + num1 + " " + num2 + " " + num3)
+
+var name1 = "<br> URDU"
+var num1 = 100;
+var num2 = 48;
+var num3 = "48%";
+document.write(name1 + " " + num1 + " " + num2 + " " + num3)
+
+
+
+
